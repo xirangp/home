3166175de35c6269eea9dc1fbfeb7c6e24c91f49
兮瀼 <100986411+xirangp@users.noreply.github.com>
2025-03-23 23:37:45 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -25,13 +25,7 @@
       >
         <component :is="store.mobileOpenState ? CloseSmall : HamburgerButton" />
       </Icon>
-      <!-- 页脚 -->
-      <Transition name="fade" mode="out-in">
-        <Footer class="f-ter" v-show="!store.backgroundShow && !store.setOpenState" />
-      </Transition>
-    </main>
-  </Transition>
-</template>
+  
 
 <script setup>
 import { helloInit, checkDays } from "@/utils/getTime.js";
