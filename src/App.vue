79d56322b33a186b051f44faffd44e8e485640c7
兮瀼 <100986411+xirangp@users.noreply.github.com>
2025-03-24 00:29:03 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
           <MainRight v-show="!store.boxOpenState" />
           <Box v-show="store.boxOpenState" />
         </section>
-        <section class="more" v-show="store.setOpenState" @click="store.setOpenState = true">
+        <section class="more" v-show="store.setOpenState" @click="store.setOpenState = false">
           <MoreSet />
         </section>
       </div>
